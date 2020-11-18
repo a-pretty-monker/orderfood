77193cd2896ed5b0fb81f5b1fb0d7ea46d407947
unknown <unknown@example.com>
2020-11-18 22:02:21 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,105 @@
+//index.js
+const app = getApp()
+const db = wx.cloud.database()
+
+Page({
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+  },
+
+
+  onGetUserInfo: function(e) {
+    console.log(e);
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        app.globalData.name = e.detail.userInfo.nickName
+        app.globalData.avatarUrl = e.detail.userInfo.avatarUrl
+        wx.switchTab({
+          url: '../index1/index1',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.hideLoading();
+        wx.switchTab({
+          url: '../index/index',
+        })
+      }
+    })
+  },
+
+
+  // // 上传图片
+  // doUpload: function () {
+  //   // 选择图片
+  //   wx.chooseImage({
+  //     count: 1,
+  //     sizeType: ['compressed'],
+  //     sourceType: ['album', 'camera'],
+  //     success: function (res) {
+
+  //       wx.showLoading({
+  //         title: '上传中',
+  //       })
+
+  //       const filePath = res.tempFilePaths[0]
+        
+  //       // 上传图片
+  //       const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+  //       wx.cloud.uploadFile({
+  //         cloudPath,
+  //         filePath,
+  //         success: res => {
+  //           console.log('[上传文件] 成功：', res)
+
+  //           app.globalData.fileID = res.fileID
+  //           app.globalData.cloudPath = cloudPath
+  //           app.globalData.imagePath = filePath
+            
+  //           wx.navigateTo({
+  //             url: '../storageConsole/storageConsole'
+  //           })
+  //         },
+  //         fail: e => {
+  //           console.error('[上传文件] 失败：', e)
+  //           wx.showToast({
+  //             icon: 'none',
+  //             title: '上传失败',
+  //           })
+  //         },
+  //         complete: () => {
+  //           wx.hideLoading()
+  //         }
+  //       })
+
+  //     },
+  //     fail: e => {
+  //       console.error(e)
+  //     }
+  //   })
+  // },
+  
+
+})
